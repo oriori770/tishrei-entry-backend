6797a93b3b748cbf300056985cacc93e6b06882f
SHNEOR ZALMAN RAINITZ <99143469+oriori770@users.noreply.github.com>
2025-08-06 10:06:34 +0300

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
 
+console.log('env:', process.env);
 const MONGODB_URI = process.env.MONGODB_URI //|| 'mongodb://localhost:27017/tishrei-entry';
 
 export const connectDB = async (): Promise<void> => {
