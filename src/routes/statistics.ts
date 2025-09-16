62bf141a3c402a506a16cb1a2421997f73c83b5c
SHNEOR ZALMAN RAINITZ <99143469+oriori770@users.noreply.github.com>
2025-09-16 07:48:26 +0300

    Update statistics.ts

--- src/routes/statistics.ts
+++ src/routes/statistics.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import {
   getEventAttendance,
   getEventsAttendance,
+  getAllEntriesByBucket,
+  getEventEntries
 } from '../controllers/statisticsController';
 import { authenticateToken, requireAdmin } from '../middleware/auth';
 
@@ -13,5 +15,11 @@ router.use(authenticateToken);
 // סטטיסטיקות נוכחות
 router.get('/event/:eventId', requireAdmin, getEventAttendance); // נוכחות לאירוע מסוים
 router.get('/events', requireAdmin, getEventsAttendance);
+
+router.get("/event/:eventId", requireAdmin, getEventEntries);
+
+// כל האירועים - קיבוץ ל־5 דקות
+router.get("/buckets", requireAdmin, getAllEntriesByBucket);
+
 
 export default router;
